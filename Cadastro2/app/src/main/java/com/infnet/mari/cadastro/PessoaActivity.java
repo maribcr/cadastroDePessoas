b5a302af3ebd69ae15ba067867533a0c3a2f4299
Mariana Barros <mariana.barros@globoesporte.com>
2017-10-09 02:08:11 -0300

    Initial commit
    cadastro de pessoas tp3 desenvolvimento android

---
+++ Cadastro2/app/src/main/java/com/infnet/mari/cadastro/PessoaActivity.java
@@ -0,0 +1,11 @@
+package com.infnet.mari.cadastro;
+
+import android.os.Bundle;
+
+class PessoaActivity extends MainActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadpessoas);
+    }
+}
